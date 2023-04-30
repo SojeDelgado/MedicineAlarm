ea6d94066a6b5d66354b7122e52cbf2dbe784a97
SojeDelgado <delgadocoreeajosemiguel@gmail.com>
2023-04-29 19:08:32 -0600

    Ya se puede editar, borrar y agregar desde la base de datps :)

--- app/src/main/java/com/example/ejemplorecyclerview/MainActivity.java
+++ app/src/main/java/com/example/ejemplorecyclerview/MainActivity.java
@@ -109,5 +109,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(listAdapter);
+
     }
 }
